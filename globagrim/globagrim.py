6dde554fedcf6b01f669bde8be4c5959a7699f22
Marco Kulueke <marco.kulueke@web.de>
2021-04-16 10:45:00 +0200

    fix path bug

--- globagrim/globagrim.py
+++ globagrim/globagrim.py
@@ -1,9 +1,9 @@
 from .variables import global_const, global_int, global_array
-import grid
-import init
-import boundary_conditions
-import trend
-import output
+import .grid
+import .init
+import .boundary_conditions
+import .trend
+import .output
 
 
 def globagrim():
